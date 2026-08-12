0c8dda00a71b5da16fe051b372c5543051b95999
Ankita Bhattacharjee <ankitab.priv@gmail.com>
2026-08-13 02:42:43 +0530

    Time: 0 ms (100.00%) | Memory: 8.6 MB (39.06%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.cpp
+++ 70-climbing-stairs/climbing-stairs.cpp
@@ -1,13 +1,13 @@
 class Solution {
+private:
+    int solve(int n, vector<int>& memo){
+        if(n <= 2) return n;
+        if(memo[n] != -1) return memo[n];
+        return memo[n] = solve(n-1, memo) + solve(n-2, memo);
+    }
 public:
     int climbStairs(int n) {
-        if(n <= 2) return n;
-        vector<int> dp(n + 1);
-        dp[1] = 1;
-        dp[2] = 2;
-        for(int i = 3; i <= n; i++){
-            dp[i] = dp[i-1] + dp[i-2];
-        }
-        return dp[n];
+        vector<int> memo(n + 1, -1);
+        return solve(n, memo);
     }
 };
